8f04bb6470d7112a70fddf3791de4bfe11d98e54
jbmahaffey <84487961+jbmahaffey@users.noreply.github.com>
2022-08-25 10:05:01 -0700

    update

--- policy.tf
+++ policy.tf
@@ -4,7 +4,15 @@ resource "fortimanager_packages_pkg" "terrapackage" {
   adom = "Test"
 }
 
-resource "fortimanager_packages_firewall_policy" "labelname" {
+resource "fortimanager_object_webfilter_profile" "tf_webfilter" {
+  comment = "This is a Terraform example"
+  name = "terr-webfilter-profile"
+  feature_set = "proxy"
+  log_all_url = "disable"
+  https_replacemsg = "enable"
+}
+
+resource "fortimanager_packages_firewall_policy" "tf_policy" {
     action = "accept"
     srcaddr = ["${fortimanager_object_firewall_addrgrp.testobj.name}"]
     dstaddr = ["all"]
